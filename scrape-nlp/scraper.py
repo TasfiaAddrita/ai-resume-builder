585ce1de5d0f6dae1b6949ddbe80f06c3c68c0ba
Aleia <aleiaknight@gmail.com>
2021-01-09 11:07:56 -0700

    [add] add code to .py to read user input url

--- scrape-nlp/scraper.py
+++ scrape-nlp/scraper.py
@@ -1,5 +1,21 @@
 from bs4 import BeautifulSoup
+import requests
 
-soup = BeautifulSoup(open("repos.html"), features="lxml")
+# Test URL https://www.usajobs.gov/GetJob/ViewDetails/576145500#locations
 
-print(soup.prettify())
+# with open("nasa.htm") as html_file:
+#     soup = BeautifulSoup(html_file, 'lxml')
+# print(soup.prettify())
+input = input('Enter URL: ')
+url = requests.get(input).text
+soup = BeautifulSoup(url, 'lxml')
+
+
+title = soup.find('h1').text
+dept = soup.find('h5', class_='usajobs-joa-banner__dept').text
+loc = soup.find('a', class_='usajobs-joa-banner__agency').text
+print(title, dept, loc)
+
+for header in soup.find_all('h2', class_='usajobs-joa-section__header'):
+    head = header.text
+    print(head)
