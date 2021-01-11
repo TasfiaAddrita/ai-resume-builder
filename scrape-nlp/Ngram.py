5d75da61b39415d4dc27286077659f6e95759355
Aleia <aleiaknight@gmail.com>
2021-01-11 01:25:57 -0700

    [update] consolidated into 1 script

--- scrape-nlp/Ngram.py
+++ scrape-nlp/Ngram.py
@@ -3,38 +3,42 @@ from random import randrange, choice
 # words = "Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind."
 # words = "how much wood would a woodchuck chuck if a woodchuck could chuck wood."
 
-with open('scrape-nlp/nasa-test.txt', 'r') as file:
+with open('nasa-test.txt', 'r') as file:
     words = file.read().replace('\n', '')
 
 
 order = 6
 ngrams= {}
 
-
-# MAIN NGRAM FUNCTION
-i=0
-for i in range(len(words)-order):
-    grams = words[i:i+order]   #takes in 3 characters at a time
-    # ngrams.setdefault(grams, 0) # Insert key with a value of default if key is not in the dictionary
-    # ngrams[grams]+=1  # increase value count if found again
-    if grams not in ngrams.keys():
-        ngrams[grams]=[] #make empty if not in ngram
-    ngrams[grams].append(words[i+order]) #print next possible char (from ngram[gram[0]])
+def ngram():
+    # MAIN NGRAM FUNCTION
+    i=0
+    for i in range(len(words)-order):
+        grams = words[i:i+order]   #takes in 3 characters at a time
+        # ngrams.setdefault(grams, 0) # Insert key with a value of default if key is not in the dictionary
+        # ngrams[grams]+=1  # increase value count if found again
+        if grams not in ngrams.keys():
+            ngrams[grams]=[] #make empty if not in ngram
+        ngrams[grams].append(words[i+order]) #print next possible char (from ngram[gram[0]])
 
 # [print(key, value) for key, value in ngrams.items()]  # printing all ngrams
 # print(n_gram(words, order))    #for function refactoring 
 
 
-
 # SENTENCE GENERATION
-test = 200
-current_gram = words[0:order]  #start at beginning
-result = current_gram
-for i in range(test):
-    if current_gram not in ngrams.keys():   # keyError handling
-        break
-    possibilities = ngrams[current_gram]  #all possibilities
-    _next = possibilities[randrange(len(possibilities))]   #choose a random one from possibilities
-    result += _next   #add to result
-    current_gram = result[len(result)-order:len(result)]   # next 3 of the currently picked gram
-print(result)
+def generate():
+    test = 2000
+    current_gram = words[0:order]  #start at beginning
+    result = current_gram
+    for i in range(test):
+        if current_gram not in ngrams.keys():   # keyError handling
+            break
+        possibilities = ngrams[current_gram]  #all possibilities
+        _next = possibilities[randrange(len(possibilities))]   #choose a random one from possibilities
+        result += _next   #add to result
+        current_gram = result[len(result)-order:len(result)]   # next 3 of the currently picked gram
+    print("\n\n Here is your generated resume:\n\n\n" , result)
+
+if __name__ == '__main__':
+    ngram()
+    generate()
--- scrape-nlp/scraper.py
+++ scrape-nlp/scraper.py
@@ -3,36 +3,44 @@ import requests
 
 # Test URL https://www.usajobs.gov/GetJob/ViewDetails/576145500#locations
 
+def scrape():
+    url = input('Enter URL: ')
+    url = requests.get(url).text
+    soup = BeautifulSoup(url, 'lxml')
 
-input = input('Enter URL: ')
-url = requests.get(input).text
-soup = BeautifulSoup(url, 'lxml')
+    # Scrape data from url
+    title = soup.find('h1').text
+    dept = soup.find('h5', class_='usajobs-joa-banner__dept').text
+    loc = soup.find('a', class_='usajobs-joa-banner__agency').text
+    summary_title = soup.find(id='duties').h3.text #summary title
+    summary_text = soup.find(id='duties').p.text #summary text
+    response_title = soup.findAll('h3')[2].text #responsibilities title
+    response_text = soup.findAll(id='duties')[1].text #responsibilities text
+    qual = soup.find(id="qualifications").text # qualifications title and text
+    edu_title = soup.find(id="requirements").findAll('div')[2].h3.text #education title
+    edu_text = soup.find(id="requirements").findAll('div')[2].p.text  #education text
 
-# Scrape data from url
-title = soup.find('h1').text
-dept = soup.find('h5', class_='usajobs-joa-banner__dept').text
-loc = soup.find('a', class_='usajobs-joa-banner__agency').text
-summary_title = soup.find(id='duties').h3.text #summary title
-summary_text = soup.find(id='duties').p.text #summary text
-response_title = soup.findAll('h3')[2].text #responsibilities title
-response_text = soup.findAll(id='duties')[1].text #responsibilities text
-qual = soup.find(id="qualifications").text # qualifications title and text
-edu_title = soup.find(id="requirements").findAll('div')[2].h3.text #education title
-edu_text = soup.find(id="requirements").findAll('div')[2].p.text  #education text
+    # Print to console
+    print(title, dept, loc)
+    print(summary_title, end='\n\n')
+    print(summary_text, end='\n\n')
+    print(response_title)
+    print(response_text)
+    print(qual)
+    print(edu_title)
+    print(edu_text)
 
-# Print to console
-print(title, dept, loc)
-print(summary_title, end='\n\n')
-print(summary_text, end='\n\n')
-print(response_title)
-print(response_text)
-print(qual)
-print(edu_title)
-print(edu_text)
+    # Save to txt file
+    file = open("nasa-test.txt", "w+")
+    datas = [title, dept, loc, summary_title, summary_text, response_title, response_text, qual, edu_title, edu_text]
+    for data in datas:
+        file.write(data)
+    file.close()
 
-# Save to txt file
-file = open("nasa-test.txt", "x")
-datas = [title, dept, loc, summary_title, summary_text, response_title, response_text, qual, edu_title, edu_text]
-for data in datas:
-    file.write(data)
-file.close()
+
+
+if __name__ == '__main__':
+    scrape()
+    import Ngram
+    Ngram.ngram()
+    Ngram.generate()
