16b0b54fb08ed7b59c7c4253102f6d21af920f9e
Aleia <aleiaknight@gmail.com>
2021-01-10 21:57:00 -0700

    [add] save data to file

--- scrape-nlp/scraper.py
+++ scrape-nlp/scraper.py
@@ -8,15 +8,31 @@ input = input('Enter URL: ')
 url = requests.get(input).text
 soup = BeautifulSoup(url, 'lxml')
 
-
+# Scrape data from url
 title = soup.find('h1').text
 dept = soup.find('h5', class_='usajobs-joa-banner__dept').text
 loc = soup.find('a', class_='usajobs-joa-banner__agency').text
+summary_title = soup.find(id='duties').h3.text #summary title
+summary_text = soup.find(id='duties').p.text #summary text
+response_title = soup.findAll('h3')[2].text #responsibilities title
+response_text = soup.findAll(id='duties')[1].text #responsibilities text
+qual = soup.find(id="qualifications").text # qualifications title and text
+edu_title = soup.find(id="requirements").findAll('div')[2].h3.text #education title
+edu_text = soup.find(id="requirements").findAll('div')[2].p.text  #education text
+
+# Print to console
 print(title, dept, loc)
-print(soup.find(id='duties').h3.text, end='\n\n') #summary title
-print(soup.find(id='duties').p.text, end='\n\n') #summary text
-print(soup.findAll('h3')[2].text) #responsibilities title
-print(soup.findAll(id='duties')[1].text) #responsibilities text
-print(soup.find(id="qualifications").text) # qualifications title and text
-print(soup.find(id="requirements").findAll('div')[2].h3.text) #education title
-print(soup.find(id="requirements").findAll('div')[2].p.text)  #education text
+print(summary_title, end='\n\n')
+print(summary_text, end='\n\n')
+print(response_title)
+print(response_text)
+print(qual)
+print(edu_title)
+print(edu_text)
+
+# Save to txt file
+file = open("nasa-test.txt", "x")
+datas = [title, dept, loc, summary_title, summary_text, response_title, response_text, qual, edu_title, edu_text]
+for data in datas:
+    file.write(data)
+file.close()
